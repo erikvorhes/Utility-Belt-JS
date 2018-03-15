1c7f9f89781ba9437bde496c16fafc7c5c76fdb1
Erik Vorhes <erik@Ericas-Mac-mini-424.local>
2018-03-15 00:23:29 -0700

    Updated to use a handy ES2015 class, mostly because it's a ton clearer.

--- README.md
+++ README.md
@@ -1,18 +1,19 @@
 # Handy JavaScript without the jQueries, et al.
 
-Not much to say here, but **utility-belt.js** is a simple wrapper for some common tasks. What does it do? Glad you asked!
+Not much to say here, but **UtilityBelt.js** is a simple wrapper to support common tasks that rely on `className`s. What does it do? Glad you asked!
 
 ## Features
 
-* Add a class to an element with `Belt.addClass(className, el);`
-* Remove a class from an element with  `Belt.removeClass(className, el);`
-* See whether an element has a certain class with `Belt.hasClass(className, el);`
-* Get all elements that have a certain class with `Belt.getElementsByClassName(className, domNode);` (`domNode` defaults to `document`)
-* Add an event listener with `Belt.newEventListener(el, evt, callback, capture);` (`capture` defaults to `false`)
-* Fire a function when the DOM has loaded (before `window.onload`) with `Belt.domReady(function);`
+* Instantiate with `const Belt = new UtilityBelt();` or whatever.
+* Add a class name to an element with `Belt.addClassName(className, el);`.
+* Remove a class name from an element with  `Belt.removeClassName(className, el);`
+* See whether an element has a certain class name with `Belt.hasClassName(className, el);`.
+* Get all elements that have a certain class name with `Belt.getElementsByClassName(className, element);` (`element` defaults to `document`).
+* Add an event listener with `Belt.addEventListener(eventTarget, event, callback, capture);` (`capture` defaults to `false`).
+* Fire a function when the DOM has loaded (before `window.onload`) with `Belt.onDOMReady(callback);`.
 
-That's it so far.
+**N.b.:** This set of utilities is offered as-is (for now). Eventually I *might* get it set up for use with Babel or other tools that convert ES2015 classes into objects and handle arrow functions. Right now this project exists as an intellectual exercise and to keep track of current and historical approaches to dealing with class names in JavaScript.
 
-Licensed using the [MIT License][mit]
+Licensed using the [MIT License][mit].
 
 [mit]: https://github.com/erikvorhes/mit-license
---
+++ UtilityBelt.js
@@ -0,0 +1,111 @@
+"use strict";
+
+class UtilityBelt {
+
+  constructor() {
+    this.__supportsAddEventListener = !!document.addEventListener;
+    this.__supportsAttachEvent = !!document.attachEvent;
+
+    const supportsClassList = () => {
+      const div = document.createElement("div");
+      return !!div.classList;
+    };
+    this.__supportsClassList = supportsClassList();
+
+    this.__supportsGetElementsByClassName = !!document.getElementsByClassName;
+    this.__supportsQuerySelectorAll = !!document.querySelectorAll;
+  }
+
+  findClassName(className) {
+    return new RegExp("(^|\\s)" + className + "(\\s|$)", "i");
+  }
+
+  hasClassName = (className, element) => {
+    if (this.__supportsClassList) {
+      return element.classList.contains(className);
+    }
+
+    const findClassName = this.findClassName(className);
+    return element.className && findClassName.test(element.className);
+  }
+
+  addClassName = (className, element) => {
+    if (this.__supportsClassList) {
+      element.classList.add(className);
+    } else {
+      element.className += ` ${className}`;
+    }
+  }
+
+  removeClassName = (className, element) => {
+    if (this.__supportsClassList) {
+      element.classList.remove(className);
+    } else {
+      const removeClass = this.findClassName(className);
+      element.className = element.className.replace(removeClass, "$1$2");
+    }
+  }
+
+  getElementsByClassName = (className, element = document) => {
+    // Faster than element.querySelectorAll()
+    if (this.__supportsGetElementsByClassName) {
+      return element.getElementsByClassName(className);
+    }
+
+    // Waaaaay faster than the last resort
+    if (this.__supportsQuerySelectorAll) {
+      return element.querySelectorAll(`.${className}`);
+    }
+
+    const descendantsWithClass = [];
+    element.getElementsByTagName("*").forEach((node) => {
+      if (this.hasClass(className, node)) {
+        descendantsWithClass.push(node);
+      }
+    });
+
+    return descendantsWithClass;
+  }
+
+  /**
+   * Wrap addEventListener and the old IE approach to save some redundancy.
+   * Note that `this` is different in an event fired in IE's elem.attachEvent --
+   * maybe @see
+   * https://developer.mozilla.org/en-US/docs/Web/API/EventTarget/attachEvent --
+   * so be careful if you need to support really old browsers.
+   */
+  addEventListener = (eventTarget, event, callback, capture) {
+    if (this.__supportsAddEventListener) {
+      eventTarget.addEventListener(event, callback, !!capture);
+    } else if (this.__supportsAttachEvent) {
+      eventTarget.attachEvent(`on${event}`, callback);
+    } else {
+      console && console.warn('`newEventListener(...)` is unsupported.');
+    }
+
+
+  }
+
+  onDOMReady = (callback) => {
+    const DOM_LOADED = "DOMContentLoaded";
+    const ON_READY = "onreadystatechange";
+
+    if (this.__supportsAddEventListener) {
+      document.addEventListener(DOM_LOADED, () => {
+        document.removeEventListener(DOM_LOADED, arguments.callee, false);
+        callback();
+      });
+    } else if (this.__supportsAttachEvent) {
+      document.attachEvent(ON_READY, () => {
+        if (document.readyState === "complete") {
+          document.detachEvent(ON_READY, arguments.callee);
+          callback();
+        }
+      });
+    } else {
+      console && console.warn('`onDOMReady(...)` is unsupported.');
+    }
+  }
+}
+
+export default UtilityBelt;
